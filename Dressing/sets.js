2fdc19cea1a63990b91a88733ca813254799e654
Albert <alhafram@gmail.com>
2021-12-11 16:40:24 +0300

    Refactor

--- Dressing/sets.js
+++ Dressing/sets.js
@@ -1,119 +1,76 @@
-let setsState = {
-    sets: null,
-    currentSet: {
-        id: null,
-        box: null,
-        items: null
-    }
-}
+class SetManager {
+    sets = []
+    currentSet = null
 
-document.addEventListener("LoadSets", event => {
-    let sets = Object.keys(localStorage).filter(key => key.startsWith("set_"))
-    if(!sets.isEmpty()) {
-        for(var set of sets) {
-            let jsonSet = JSON.parse(localStorage[set])
-            let article = document.createElement('article')
+    #article = "leaderboard__profile"
+    #activeArticle = "leaderboard__profile active"
 
-            article.id = set
-            article.className = "leaderboard__profile"
-            article.onclick = function(e) {
-                chooseSet(this)
-            }
-
-            let img = document.createElement("img")
-            img.src = "https://cdn-icons-png.flaticon.com/512/599/599502.png"
-            img.className = "leaderboard__picture"
-            article.appendChild(img)
-
-            let span = document.createElement("span")
-            span.className = "leaderboard__name"
-            span.textContent = jsonSet.title
-            article.appendChild(span)
-            document.querySelector(".sets").appendChild(article)
-        }
-        setsState.currentSet.id = sets[0]
-    }
-})
-
-function chooseSet(element) {
-    if(element.className == "leaderboard__profile  active") {
-        return
-    }
-    deselectOtherSets()
-    element.className = "leaderboard__profile  active"
-    var jsonSet = null
-    if(element.id) {
-        jsonSet = JSON.parse(localStorage[element.id])
-        document.querySelector("#currentSetTitle").value = jsonSet.title
+    get setsBox() {
+        return document.querySelector(".sets")
     }
 
-    setsState.currentSet.box = element
-    unequipFromSet()
-    if(jsonSet) {
-        equipFromSet(jsonSet.ids)
-    }
-}
-
-function equipFromSet(ids) {
-    for(var id of ids) {
-        let items = Array.from(document.querySelector(".current_items").children)
-        let item = items.filter(element => element.attributes.itemid.value == id)[0]
-        if(item) {
-            state.currentElement = item
-            putOnItem(item)
-            state.currentElement = null
+    pushSet(newSet) {
+        let set = this.sets.find(e => e.id == newSet.id)
+        if(!isExists(set)) {
+            this.sets.push(newSet)
         } else {
-            console.log("SOMETHING WRONG", id)
-            return
+            this.sets[this.sets.indexOf(set)] = newSet
         }
     }
-}
 
-function unequipFromSet() {
-    let items = state.getEquipedItems()
-    console.log(items)
-    for(var item of items) {
-        putOffItem(item.parentElement, false, true)
-        let copys = Array.from(document.querySelector(".current_items").children).filter(i => i.attributes.copy)
-        for(var copy of copys) {
-            copy.parentElement.removeChild(copy)
+    setupListeners() {
+        let self = this
+        document.querySelector("#addSetButton").addEventListener('click', e => {
+            this.deselectOtherArticles()
+            self.unequip()
+            let randomId = (Math.random() + 1).toString(36).substring(2)
+            let id = "set_" + randomId
+            let newSet = {
+                id: id,
+                title: "Default set",
+                ids: []
+            }
+            let article = this.createSetArticleElement(null, true)
+            document.querySelector("#currentSetTitle").value = "Default set"
+            this.setsBox.insertBefore(article, this.setsBox.firstElementChild.nextSibling)
+            self.currentSet = newSet
+            self.pushSet(newSet)
+        })
+
+        document.querySelector("#currentSetTitle").addEventListener('change', e => {
+            // TODO
+        })
+        document.querySelector("#saveSet").addEventListener('click', e => {
+            self.saveSet()
+        })
+        document.querySelector("#unequip").addEventListener('click', e => {
+            self.unequip()
+        })
+    }
+
+    loadSets() {
+        const setKeys = Object.keys(localStorage).filter(key => key.startsWith("set_"))
+        let sets = setKeys.map(key => JSON.parse(localStorage[key]))
+        this.sets = sets
+    }
+
+    fillSets() {
+        if(!this.sets.isEmpty()) {
+            for(var set of this.sets) {
+                let article = this.createSetArticleElement(set)
+                this.setsBox.appendChild(article)
+            }
         }
     }
-    // console.log(items)
-}
 
-function deselectOtherSets() {
-    let needToDeselectElements = Array.from(document.querySelector(".sets").children).filter(i => i.id != "addSetButton")
-    for(var element of needToDeselectElements) {
-        console.log("DESELECTED")
-        element.className = "leaderboard__profile"
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.querySelector("#currentSetTitle").addEventListener('change', e => {
-        if(!setsState.currentSet.box) {
-            return
-        }
-        setsState.currentSet.box.lastElementChild.textContent = e.target.value
-    })
-
-    document.querySelector("#addSetButton").addEventListener('click', e => {
-        let sets = Object.keys(localStorage)
-        let emptySet = sets.filter(i => JSON.parse(localStorage[i]).ids.isEmpty())[0]
-        if(emptySet) {
-            alert(`Такой набор вещей уже существует: ${JSON.parse(localStorage[emptySet]).title}`)
-            return
-        }
-        deselectOtherSets()
-        unequipFromSet()
+    createSetArticleElement(set, active) {
         let article = document.createElement('article')
-        article.className = "leaderboard__profile  active"
-        article.draggable = 'true'
-
+        article.id = isExists(set) ? set.id : "SOME RANDOM ID"
+        let className = active ? this.#activeArticle : this.#article
+        article.className = className
+        var self = this
         article.onclick = function(e) {
-            chooseSet(this)
+            self.selectSet(this)
         }
 
         let img = document.createElement("img")
@@ -123,49 +80,100 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let span = document.createElement("span")
         span.className = "leaderboard__name"
-        span.textContent = "Default set"
+        span.textContent = isExists(set) ? set.title : "Default set"
         article.appendChild(span)
+        return article
+    }
 
-        setsState.currentSet.box = article
-        setsState.currentSet.id = null
-        document.querySelector("#currentSetTitle").value = "Default set"
-        document.querySelector(".sets").insertBefore(article, document.querySelector(".sets").firstElementChild.nextSibling)
-        saveSet()
-    })
+    selectSet(element) {
+        if(element.className == this.#activeArticle) {
+            return
+        }
+        this.deselectOtherArticles()
+        this.unequip()
+        element.className = this.#activeArticle
+        let selectedSet = this.sets.find(obj => obj.id == element.id)
+        if(isExists(selectedSet)) {
+            this.currentSet = selectedSet
+            this.equipFromSet(selectedSet.ids)
+            document.querySelector("#currentSetTitle").value = selectedSet.title
+        } else {
+            document.querySelector("#currentSetTitle").value = "Default set"
+            this.unequip()
+        }
+    }
+
+    saveSet() {
+        let items = state.getEquipedItems()
+        let title = document.querySelector("#currentSetTitle").value
+        let ids = items.map(i => i.attributes.itemid.value)
+
+        let setArticles = setManager.setsBox.children.toArray()
+        let activeSet = setArticles.filter(e => e.id != "addSetButton" && e.className == this.#activeArticle).first()
+        let id = null
+        var isNew = true
+        if(isExists(activeSet) && isExists(this.currentSet)) {
+            id = this.currentSet.id
+            isNew = false
+            activeSet.lastElementChild.textContent = title
+        } else {
+            let randomId = (Math.random() + 1).toString(36).substring(2)
+            id = "set_" + randomId
+        }
+        let newSet = {
+            id: id,
+            title: title,
+            ids: ids.unique()
+        }
+        activeSet.id = id
+        localStorage.setItem(id, JSON.stringify(newSet))
+        this.pushSet(newSet)
+        this.currentSet = newSet
+        if(isNew) {
+            let article = this.createSetArticleElement(newSet, true)
+            this.setsBox.insertBefore(article, this.setsBox.firstElementChild.nextSibling)
+        }
+    }
+
+    deselectOtherArticles() {
+        let self = this
+        this.setsBox.children.toArray().forEach(e => {
+            e.className = self.#article
+        })
+    }
+
+    unequip() {
+        let items = state.getEquipedItems()
+        console.log(items)
+        for(var item of items) {
+            putOffItem(item.parentElement, false, true)
+            let copys = Array.from(document.querySelector(".current_items").children).filter(i => i.attributes.copy)
+            for(var copy of copys) {
+                copy.parentElement.removeChild(copy)
+            }
+        }
+    }
+
+    equipFromSet(ids) {
+        for(var id of ids) {
+            let items = Array.from(document.querySelector(".current_items").children)
+            let item = items.filter(element => element.attributes.itemid.value == id)[0]
+            if(item) {
+                state.currentElement = item
+                putOnItem(item)
+                state.currentElement = null
+            } else {
+                console.log("SOMETHING WRONG", id)
+                return
+            }
+        }
+    }
+}
+
+let setManager = new SetManager()
+
+document.addEventListener("LoadSets", event => {
+    setManager.loadSets()
+    setManager.fillSets()
+    setManager.setupListeners()
 })
-
-function saveSet(e) {
-    if(!setsState.currentSet.box) {
-        return
-    }
-    let items = state.getEquipedItems()
-    let title = setsState.currentSet.box.lastElementChild.textContent
-    let ids = items.map(i => i.attributes.itemid.value)
-    let str = ids.join(';')
-    str += title
-    var id = null
-    if(setsState.currentSet.id) {
-        id = setsState.currentSet.id
-        localStorage.removeItem(id)
-        id = str.hashCode()
-        id = "set_" + id
-    } else {
-        id = str.hashCode()
-        id = "set_" + id
-    }
-    localStorage.setItem(id, JSON.stringify({title: title, ids: Array.from(new Set(ids))}))
-    setsState.currentSet.id = id
-    setsState.currentSet.box.id = id
-}
-
-String.prototype.hashCode = function(seed = 0){
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < this.length; i++) {
-        ch = this.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-    return 4294967296 * (2097151 & h2) + (h1>>>0);
-}
--- utils.js
+++ utils.js
@@ -1,3 +1,31 @@
 Array.prototype.isEmpty = function() {
     return this.length == 0
 }
+
+String.prototype.hashCode = function(seed = 0){
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < this.length; i++) {
+        ch = this.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1>>>0);
+}
+
+function isExists(element) {
+    return element !== null && element !== undefined
+}
+
+HTMLCollection.prototype.toArray = function() {
+    return Array.from(this)
+}
+
+Array.prototype.first = function() {
+    return this[0]
+}
+
+Array.prototype.unique = function () {
+    return Array.from(new Set(this))
+};
