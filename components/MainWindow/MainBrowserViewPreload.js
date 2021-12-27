79c31de2cfeb36deed462dd6650d1c7bd375c84d
Albert <alhafram@gmail.com>
2021-12-27 23:42:22 +0300

    Add chat logger

---
+++ components/MainWindow/MainBrowserViewPreload.js
@@ -0,0 +1,20 @@
+const { checkLmtsProxyReady, setupChatTotalReconnect, setupChatInterval, setupReceiver } = require('../Chat/Chat')
+
+window.addEventListener('DOMContentLoaded', async () => {
+
+    let firstInterval = setInterval(async () => {
+        if(!top[1] || !top[1].LMTS) {
+            return
+        }
+        if(top[1].LMTS.isConnected() && top[1].LMTS.isAuthorized()) {
+            setupReceiver()
+            checkLmtsProxyReady()
+            clearInterval(firstInterval)
+            console.log(new Date(), "DONE 2")
+            setupChatTotalReconnect()
+            setupChatInterval()
+        }
+    }, 100)
+
+    console.log(new Date(), "DONE 1")
+})
