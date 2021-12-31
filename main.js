15e6c06bd87e75e7dbbc2efa9764106800a01047
Albert <alhafram@gmail.com>
2022-01-01 00:26:05 +0300

    Script fixes

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ const { autoUpdater } = require("electron-updater")
 
 autoUpdater.checkForUpdatesAndNotify()
 
+autoUpdater.signals.updateDownloaded(info => {
+    console.log("DOWNLOADED", info)
+    mainWindow.webContents.send('updateApplicationAvailable')
+})
+
 let mainWindow
 
 function createWindow() {
@@ -195,6 +200,10 @@ function createWindow() {
             url = 'https://' + url
         }
         TabsController.currentTab().webContents.loadURL(url)
+    })
+
+    ipcMain.on('updateApplication', () => {
+        autoUpdater.quitAndInstall(false, true)
     })
 
     ipcMain.handle('LoadSetItems', async (evt, args) => {
