005179c55a5244503a5b83c699e22f5be91cb53a
Albert <alhafram@gmail.com>
2022-02-04 20:48:11 +0300

    Refactor user config on notes

--- src/components/Notes/preload.ts
+++ src/components/Notes/preload.ts
@@ -1,27 +1,20 @@
 import { contextBridge } from 'electron'
-import configService from '../../services/ConfigService'
 import NotesService from '../../services/NotesService'
 
 contextBridge.exposeInMainWorld('notesAPI', {
-    // Refactor 1.0.15
-    loadOldNotes: () => {
-        return configService.notes()
-    },
-    loadNewNotes: () => {
+    loadNotes: () => {
         return NotesService.notes()
     },
     saveNote: (note: any) => {
         NotesService.writeData(note.id, JSON.stringify(note))
     },
     removeNote: (id: string) => {
-        configService.writeData(id, null)
         NotesService.writeData(id, null)
     }
 })
 
 export interface NotesAPI {
-    loadOldNotes: () => any,
-    loadNewNotes: () => any,
+    loadNotes: () => any,
     saveNote: (note: {}) => void,
     removeNote: (id: string) => void
 }
