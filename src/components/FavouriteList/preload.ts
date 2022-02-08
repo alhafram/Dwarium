32808b619c643dbe2e73d9498ca777b59a835c54
Albert <alhafram@gmail.com>
2022-02-08 21:01:23 +0300

    Fix

--- src/components/FavouriteList/preload.ts
+++ src/components/FavouriteList/preload.ts
@@ -49,12 +49,13 @@ function renderFavouriteLinks() {
     favouriteLinks.forEach(link => {
         const title = link.title.slice(0, 18)
         const url = link.url.slice(0, 20)
+        const right = process.platform == 'win32' ? '20px' : '12px'
         let favouriteLinkElement = `<div class=" h-9 w-full hover:bg-inputBackgroundColor dark:hover:bg-inputBackgroundColorDark">
                 <div class="float-left">
                     <p class="ml-9 text-textColor dark:text-textColorDark text-sm font-medium">${title}</p>
                     <p class="ml-9 text-textColor dark:text-textColorDark text-xs font-light">${url}</p>
                 </div>
-                <button style="right: 9px; margin-top: 5px;" class="relative float-right w-5.5 h-5.5 hover:bg-disabledButton dark:hover:bg-addTabDarkHover rounded-full">
+                <button style="right: ${right}; margin-top: 5px;" class="relative float-right w-5.5 h-5.5 hover:bg-disabledButton dark:hover:bg-addTabDarkHover rounded-full">
                     <svg class="buttonIcon" width="9" height="9" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg">
                         <path d="M9 0.906428L8.09357 0L4.5 3.59357L0.906428 0L0 0.906428L3.59357 4.5L0 8.09357L0.906428 9L4.5 5.40643L8.09357 9L9 8.09357L5.40643 4.5L9 0.906428Z" />
                     </svg>
